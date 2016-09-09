a6356c32d40c3645d81b665f5bd2c8a72217a4f3
David Vittor <davidv@booktopia.com.au>
2016-09-10 08:06:21 +1000

    Link updated

--- jcmodelweb/src/main/java/au/com/jcloud/model/Link.java
+++ jcmodelweb/src/main/java/au/com/jcloud/model/Link.java
@@ -8,13 +8,17 @@ import javax.persistence.Table;
  */
 @Entity
 @Table(name = "link")
-public class Link extends BaseBeanWithName {
+public class Link extends BaseBean {
+	protected String title;
+	protected String extract;
 	protected String url;
+	protected String tags;
 	protected int clickCount;
+	protected int rank;
 
 	@Override
 	public String toString() {
-		return super.toString() + " clickCount=" + clickCount+" url=" + url;
+		return super.toString() + " title="+title+" clickCount=" + clickCount+" url=" + url+" tags=" + tags+" rank=" + rank;
 	}
 
 	public String getUrl() {
@@ -23,5 +27,45 @@ public class Link extends BaseBeanWithName {
 
 	public void setUrl(String url) {
 		this.url = url;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getTags() {
+		return tags;
+	}
+
+	public void setTags(String tags) {
+		this.tags = tags;
+	}
+
+	public int getClickCount() {
+		return clickCount;
+	}
+
+	public void setClickCount(int clickCount) {
+		this.clickCount = clickCount;
+	}
+
+	public int getRank() {
+		return rank;
+	}
+
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+
+	public String getExtract() {
+		return extract;
+	}
+
+	public void setExtract(String extract) {
+		this.extract = extract;
 	}
 }
