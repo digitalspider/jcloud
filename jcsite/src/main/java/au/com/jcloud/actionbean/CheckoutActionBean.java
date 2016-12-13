a98eb34a249d9b11c52b436c6750b170878fbfae
David Vittor <davidv@booktopia.com.au>
2016-12-13 20:24:45 +1100

    Updated checkout

--- jcsite/src/main/java/au/com/jcloud/actionbean/CheckoutActionBean.java
+++ jcsite/src/main/java/au/com/jcloud/actionbean/CheckoutActionBean.java
@@ -21,6 +21,7 @@ import au.com.jcloud.model.CartItem;
 import au.com.jcloud.model.CreditCard;
 import au.com.jcloud.model.Product;
 import au.com.jcloud.model.User;
+import au.com.jcloud.service.UserService;
 import au.com.jcloud.util.PathParts;
 import net.sourceforge.stripes.action.DefaultHandler;
 import net.sourceforge.stripes.action.DontBind;
@@ -30,6 +31,7 @@ import net.sourceforge.stripes.action.HandlesEvent;
 import net.sourceforge.stripes.action.RedirectResolution;
 import net.sourceforge.stripes.action.Resolution;
 import net.sourceforge.stripes.action.UrlBinding;
+import net.sourceforge.stripes.integration.spring.SpringBean;
 
 /**
  * Created by david.vittor on 3/08/16.
@@ -40,10 +42,41 @@ public class CheckoutActionBean extends JCSecureActionBean {
 
 	public static final String JSP_BINDING = "/checkout";
 
+	public enum PageAction {
+		CART,
+		BILLING,
+		PURCHASE;
+
+		public static PageAction parse(String data) {
+			for (PageAction action : PageAction.values()) {
+				if (action.name().equalsIgnoreCase(data)) {
+					return action;
+				}
+			}
+			return null;
+		}
+	}
+
+	public enum CartAction {
+		ADD;
+
+		public static CartAction parse(String data) {
+			for (CartAction action : CartAction.values()) {
+				if (action.name().equalsIgnoreCase(data)) {
+					return action;
+				}
+			}
+			return null;
+		}
+	}
+
 	private Cart cart;
 	private Address address;
 	private CreditCard creditCard;
 	private String name;
+
+	@SpringBean
+	private UserService userService;
 
 	public String getCartPageJsp() {
 		return PATH_SECURE_JSP + getJSPBinding() + "/" + CART_JSP;
@@ -81,7 +114,7 @@ public class CheckoutActionBean extends JCSecureActionBean {
 	@Override
 	public Resolution action() {
 		User user = getUser();
-		List<Cart> carts = Ebean.find(Cart.class).setDisableLazyLoading(true).fetch("cartItems").fetch("user").where().eq("user",user).findList();
+		List<Cart> carts = Ebean.find(Cart.class).setDisableLazyLoading(true).fetch("cartItems").fetch("user").where().eq("user", user).findList();
 		if (carts.isEmpty()) {
 			cart = new Cart();
 			cart.setCartItems(new ArrayList<CartItem>());
@@ -94,7 +127,7 @@ public class CheckoutActionBean extends JCSecureActionBean {
 			return getDefaultResolution();
 		}
 		cart = carts.get(0);
-		if (cart.getCartItems()==null) {
+		if (cart.getCartItems() == null) {
 			cart.setCartItems(new ArrayList<CartItem>());
 		}
 
@@ -103,25 +136,25 @@ public class CheckoutActionBean extends JCSecureActionBean {
 		if (pathParts.size() > 1) {
 			String action = pathParts.get(1);
 			LOG.info("action=" + action);
-			switch (action) {
-			case ("cart"):
+			switch (PageAction.parse(action)) {
+			case CART:
 				if (pathParts.size() > 2) {
 					String cartAction = pathParts.get(2);
 					LOG.info("cartAction=" + cartAction);
-					switch (cartAction) {
-					case ("add"):
+					switch (CartAction.parse(cartAction)) {
+					case ADD:
 						int productId = pathParts.getInt(3);
 						LOG.info("adding productId=" + productId);
 						if (productId > 0) {
 							Product product = Ebean.find(Product.class).setDisableLazyLoading(true).where().idEq(Long.valueOf(productId)).findUnique();
 							LOG.info("adding product=" + product);
 							if (product != null) {
-								boolean added=false;
+								boolean added = false;
 								for (CartItem cartItem : cart.getCartItems()) {
 									if (cartItem.getProduct().getId().equals(product.getId())) {
-										cartItem.setQuantity(cartItem.getQuantity()+1);
+										cartItem.setQuantity(cartItem.getQuantity() + 1);
 										Ebean.update(cartItem);
-										added=true;
+										added = true;
 										break;
 									}
 								}
@@ -138,29 +171,23 @@ public class CheckoutActionBean extends JCSecureActionBean {
 					}
 				}
 				return getCartResolution();
-			case ("billing"):
+			case BILLING:
 				if (cart.getCartItems().size() < 1) {
 					LOG.error("No items in the cart()");
 					addGlobalValidationError("cart.no.items", user);
 					return getCartResolution();
 				}
 				name = user.getFullName();
-				List<Address> addressList = user.getAddresses();
-				if (addressList.isEmpty()) {
+				address = userService.getDefaultAddress(user);
+				if (address == null) {
 					address = new Address();
 				}
-				else {
-					address = addressList.get(0);
-				}
-				List<CreditCard> creditCardList = user.getCreditCards();
-				if (creditCardList.isEmpty()) {
+				creditCard = userService.getDefaultCreditCard(user);
+				if (creditCard == null) {
 					creditCard = new CreditCard();
 				}
-				else {
-					creditCard = creditCardList.get(0);
-				}
 				return getBillingResolution();
-			case ("purchase"):
+			case PURCHASE:
 				LOG.info("Need to validate cart()");
 				return getThankYouResolution();
 			}
@@ -222,6 +249,14 @@ public class CheckoutActionBean extends JCSecureActionBean {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public UserService getUserService() {
+		return userService;
+	}
+
+	public void setUserService(UserService userService) {
+		this.userService = userService;
 	}
 
 }
