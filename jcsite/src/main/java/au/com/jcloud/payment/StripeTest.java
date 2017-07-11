8fa225db381380f62574ea814330d024a6dba32f
digitalspider <dev@digitalspider.com.au>
2017-07-11 23:55:55 +1000

    Formatting

--- jcsite/src/main/java/au/com/jcloud/payment/StripeTest.java
+++ jcsite/src/main/java/au/com/jcloud/payment/StripeTest.java
@@ -1,5 +1,7 @@
 package au.com.jcloud.payment;
 
+import java.util.HashMap;
+
 /**
  * Example usage of Stripe java bindings.
  * See https://stripe.com/docs/libraries#java-library
@@ -15,41 +17,40 @@ package au.com.jcloud.payment;
  */
 
 import java.util.Map;
-import java.util.HashMap;
 
-import com.stripe.model.Charge;
 import com.stripe.Stripe;
-import com.stripe.exception.AuthenticationException;
-import com.stripe.exception.InvalidRequestException;
 import com.stripe.exception.APIConnectionException;
-import com.stripe.exception.CardException;
 import com.stripe.exception.APIException;
+import com.stripe.exception.AuthenticationException;
+import com.stripe.exception.CardException;
+import com.stripe.exception.InvalidRequestException;
+import com.stripe.model.Charge;
 
 public class StripeTest {
-    public static void main(String[] args) throws AuthenticationException, InvalidRequestException,
-            APIConnectionException, CardException, APIException  {
-        Stripe.apiKey = "tGN0bIwXnHdwOa85VABjPdSn8nWY7G7I"; // stripe public
-        // test key
+	public static void main(String[] args) throws AuthenticationException, InvalidRequestException,
+			APIConnectionException, CardException, APIException {
+		Stripe.apiKey = "tGN0bIwXnHdwOa85VABjPdSn8nWY7G7I"; // stripe public
+		// test key
 
-        final Map<String, Object> cardParams = new HashMap<String, Object>();
-        cardParams.put("number", "4242424242424242");
-        cardParams.put("exp_month", 12);
-        cardParams.put("exp_year", 2015);
-        cardParams.put("cvc", "123");
-        cardParams.put("name", "J Bindings Cardholder");
-        cardParams.put("address_line1", "140 2nd Street");
-        cardParams.put("address_line2", "4th Floor");
-        cardParams.put("address_city", "San Francisco");
-        cardParams.put("address_zip", "94105");
-        cardParams.put("address_state", "CA");
-        cardParams.put("address_country", "USA");
+		final Map<String, Object> cardParams = new HashMap<String, Object>();
+		cardParams.put("number", "4242424242424242");
+		cardParams.put("exp_month", 12);
+		cardParams.put("exp_year", 2019);
+		cardParams.put("cvc", "123");
+		cardParams.put("name", "J Bindings Cardholder");
+		cardParams.put("address_line1", "140 2nd Street");
+		cardParams.put("address_line2", "4th Floor");
+		cardParams.put("address_city", "San Francisco");
+		cardParams.put("address_zip", "94105");
+		cardParams.put("address_state", "CA");
+		cardParams.put("address_country", "USA");
 
-        final Map<String, Object> chargeParams = new HashMap<String, Object>();
-        chargeParams.put("amount", 100);
-        chargeParams.put("currency", "usd");
-        chargeParams.put("card", cardParams);
+		final Map<String, Object> chargeParams = new HashMap<String, Object>();
+		chargeParams.put("amount", 100);
+		chargeParams.put("currency", "usd");
+		chargeParams.put("card", cardParams);
 
-        final Charge charge = Charge.create(chargeParams);
-        System.out.println(charge);
-    }
+		final Charge charge = Charge.create(chargeParams);
+		System.out.println(charge);
+	}
 }
